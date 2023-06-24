72fabe53aa88aec69ea82672fcd340e4a8add8ef
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-24 01:26:49 -0300

    ploting graphs

--- results/main.py
+++ results/main.py
@@ -2,24 +2,13 @@ import csv
 from matplotlib import pyplot as plt
 
 
-def plot_exec_time_per_instance_graph():
+def plot_graph(title, file, types):
     x_points = []
     y_points = []
     x_label = ''
     y_label = ''
 
-
-def plot_operations_per_instance_graph():
-    pass
-
-
-def read_file_and_plot_graph():
-    x_points = []
-    y_points = []
-    x_label = ''
-    y_label = ''
-
-    with open('./data.csv') as file:
+    with open(file) as file:
         csv_reader = csv.reader(file, delimiter=',')
         line = 0
 
@@ -28,16 +17,25 @@ def read_file_and_plot_graph():
                 x_label = row[0]
                 y_label = row[1]
             else:
-                x_points.append(int(row[0]))
-                y_points.append(float(row[1]))
+                x_points.append(types[0](row[0]))
+                y_points.append(types[1](row[1]))
             line += 1
 
     plt.plot(x_points, y_points)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.title('Time x Words')
+    plt.title(title)
     plt.show()
 
 
+def plot_exec_time_per_instance_graph():
+   plot_graph('Execution Time x Instance', './exec_time.csv', [int, float])
+
+
+def plot_operations_per_instance_graph():
+    plot_graph('Operation x Instance', './operations.csv', [int, int])
+    
+
 if __name__ == '__main__':
-    read_file_and_plot_graph()
+    plot_exec_time_per_instance_graph()
+    plot_operations_per_instance_graph()
